a79fe4e4d2ffe11b0d413f46583147ab487ee928
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2019-03-04 20:58:47 -0300

    Aula 01: API Reflection e as faetas de this

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,15 @@
 class ListaNegociacoes{
 
-	constructor(armadilha) {
+	constructor(contexto,armadilha) {
 		this._negociacoes = [];
 		this._armadilha = armadilha;
+		this._contexto = contexto;
 	}
 
 	adicionaLista(negociacao){
 		this._negociacoes.push(negociacao);
-		this._armadila(this);
+		//this._armadila(this);
+		Reflect.apply(this._armadilha,this._contexto,[this]);
 	}
 
 	get negociacoes(){
@@ -18,7 +20,8 @@ class ListaNegociacoes{
 
 	esvazia(){
 		this._negociacoes = [];
-		this._armadila(this);
+		//this._armadila(this);
+		Reflect.apply(this._armadilha,this._contexto,[this]);
 	}
 }
 
